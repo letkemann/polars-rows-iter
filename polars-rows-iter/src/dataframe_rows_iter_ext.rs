0859e3b48cad0a2179db9689447abeb617a39bbe
letkemann <73347343+letkemann@users.noreply.github.com>
2024-12-12 23:42:29 +0100

    dynamic column names

--- polars-rows-iter/src/dataframe_rows_iter_ext.rs
+++ polars-rows-iter/src/dataframe_rows_iter_ext.rs
@@ -1,9 +1,18 @@
+use std::collections::HashMap;
+
 use polars::prelude::*;
 
-use crate::FromDataFrameRow;
+use crate::{ColumnNameBuilder, FromDataFrameRow};
 
 pub trait DataframeRowsIterExt<'a> {
     fn rows_iter<T>(&'a self) -> PolarsResult<Box<dyn Iterator<Item = PolarsResult<T>> + 'a>>
+    where
+        T: FromDataFrameRow<'a>;
+
+    fn rows_iter_for_columns<T>(
+        &'a self,
+        build_fn: impl FnOnce(&mut T::Builder),
+    ) -> PolarsResult<Box<dyn Iterator<Item = PolarsResult<T>> + 'a>>
     where
         T: FromDataFrameRow<'a>;
 }
@@ -13,6 +22,22 @@ impl<'a> DataframeRowsIterExt<'a> for DataFrame {
     where
         T: FromDataFrameRow<'a>,
     {
-        T::from_dataframe(self)
+        T::from_dataframe(self, HashMap::new())
+    }
+
+    fn rows_iter_for_columns<T>(
+        &'a self,
+        build_fn: impl FnOnce(&mut T::Builder),
+    ) -> PolarsResult<Box<dyn Iterator<Item = PolarsResult<T>> + 'a>>
+    where
+        T: FromDataFrameRow<'a>,
+    {
+        let mut builder = T::create_builder();
+
+        build_fn(&mut builder);
+
+        let columns = builder.build();
+
+        T::from_dataframe(self, columns)
     }
 }
